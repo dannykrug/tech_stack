46932b29b3e270929b271852f0090e8068396e64
dannykrug <dannykrug@gmail.com>
2019-01-17 11:47:16 -0500

    redux wired up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 const App = () => {
-  return(
-    <View>
-    </View>
-  )
-}
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Text>Redux App!</Text>
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
